d639aa2138f47d166e7fd0352f62223b2ea11595
ernestare <123376207+ernestare@users.noreply.github.com>
2023-01-27 20:50:58 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -16188,14 +16188,8 @@ Webflow.define('brand', module.exports = function ($) {
   }
 
   function createBadge() {
-    var $brand = $('<a class="w-webflow-badge"></a>').attr('href', 'https://webflow.com?utm_campaign=brandjs');
-    var $logoArt = $('<img>').attr('src', 'https://d3e54v103j8qbb.cloudfront.net/img/webflow-badge-icon.f67cd735e3.svg').attr('alt', '').css({
-      marginRight: '8px',
-      width: '16px'
-    });
-    var $logoText = $('<img>').attr('src', 'https://d1otoma47x30pg.cloudfront.net/img/webflow-badge-text.6faa6a38cd.svg').attr('alt', 'Made in Webflow');
-    $brand.append($logoArt, $logoText);
-    return $brand[0];
+   
+    return;
   }
 
   function ensureBrand() {
